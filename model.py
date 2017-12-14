d611cca904f586ffee5fbd563fa79aa5e995fd99
Kinga <felfalusi.kinga@gmail.com>
2017-12-14 22:04:13 +0100

    add update file function

--- model.py
+++ model.py
@@ -5,49 +5,43 @@ class ToDo(object):
     def __init__(self):
         with open("usage.txt", "r") as manual:
             self.usage = manual.read()
+        with open("my_todos.txt", "r") as my_file:
+            self.content = my_file.readlines()    
 
     def print_usage(self):
         print(self.usage)
 
     def list_tasks(self):
-        with open("my_todos.txt", "r") as my_file:
-            content = my_file.readlines()
-            if not content:
-                print("No todos for today! :)")
-            else:
-                for index, line in enumerate(content):
-                    print(str(index + 1) + " - " + line.rstrip())
+        if not self.content:
+            print("No todos for today! :)")
+        else:
+            for index, line in enumerate(self.content):
+                print(str(index + 1) + " - " + line.rstrip())
 
     def add_new_task(self):
         with open("my_todos.txt", "a") as my_file:
-            my_file.write("\n" + str(sys.argv[2]))
-    
+            if not self.content:
+                my_file.write("[ ] " + str(sys.argv[2]))
+            else:
+                 my_file.write("\n" + "[ ] " + str(sys.argv[2]))
+
     def remove_task(self):
-        my_file = open("my_todos.txt", "r")
-        content = my_file.readlines()
-        my_file.close()
-        if len(content) > 2:
-            content.remove(content[int(sys.argv[2]) - 1])
-        updated_file = open("my_todos.txt", "w")
-        for i in range(len(content)):
-            if i < len(content) - 1:
-                updated_file.write(content[i])
-            else:
-                updated_file.write(content[i].rstrip())
-        updated_file.close()
-        
+        if len(self.content) >= 2:
+            self.content.remove(self.content[int(sys.argv[2]) - 1])
+        self.update_file()
+
     def check_task(self):
-        my_file = open("my_todos.txt", "r")
-        content = my_file.readlines()
-        my_file.close()
         index_of_completed = int(sys.argv[2])
-        for i in range(len(content)):
+        for i in range(len(self.content)):
             if i == index_of_completed - 1:
-                content[i] = "[x]" + content[i][3: ]
+                self.content[i] = "[x]" + self.content[i][3: ]
+        self.update_file()
+
+    def update_file(self):
         updated_file = open("my_todos.txt", "w")
-        for i in range(len(content)):
-            if i < len(content) - 1:
-                updated_file.write(content[i])
+        for i in range(len(self.content)):
+            if i < len(self.content) - 1:
+                updated_file.write(self.content[i])
             else:
-                updated_file.write(content[i].rstrip())
+                updated_file.write(self.content[i].rstrip())
         updated_file.close()
